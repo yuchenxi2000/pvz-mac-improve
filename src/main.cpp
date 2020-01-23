3e95a677070836ed7da9eeeb2a4ee308b7ed5194
yuchenxi2000 <42840485+yuchenxi2000@users.noreply.github.com>
2020-01-23 19:28:07 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,62 @@
 #include "patch-address.hpp"
 #include "feature.hpp"
 #include "bugfix.hpp"
-Memory memory;
+
+#define MAKE_PATCH_DYLIB
+
+// 使用dll注入（编译动态库！）
+#ifdef MAKE_PATCH_DYLIB
+__attribute__((constructor))
+void patch_init(void) {
+    printf("---------------------------\n");
+    printf("PvZ Improve by YCX\n");
+    printf("Game Version 1.0.40\n");
+    printf("\n");
+    printf("BiliBili: yuchenxi0_0\n");
+    printf("GitHub: yuchenxi2000\n");
+    printf("\n");
+    
+    // attach process
+    Memory memory;
+    memory.AttachSelf();
+    
+    AllocPatchMemory(memory, 1024*64);
+    
+    // fix bug
+    BugFix bugfix(memory);
+    bugfix.FixUseCard();
+    bugfix.FixClickCard();
+    bugfix.FixMushroomWeakUp();
+    
+    // add feature
+    Feature feature(memory);
+    feature.FeatureFlagZombie();
+    feature.FeatureBucketDoorZombie();
+    feature.FeaturePeaZombie();
+    feature.FeatureExplodeNut();
+    feature.FeatureFumeshroomOneLine();
+    feature.FeatureKelpPull();
+    feature.FeatureShooter();
+    feature.FeatureSmartDigZombie();
+    feature.FeatureNutZombie();
+    feature.FeatureJalapenoZombie();
+    feature.FeatureWoguaZombie();
+    feature.FeatureFootballZombie();
+    feature.FeatureStar();
+    feature.FeatureMarigoldProduceCard();
+    feature.FeatureBlover();
+    
+    printf("Code Injected Successfully!\n");
+    printf("---------------------------\n");
+}
+// 运行时注入
+#else
 int main() {
+    Memory memory;
     // attach process
     pid_t pid = memory.PidFromName("Plants vs. Zombi");
     memory.Attach(pid);
-
+    
     AllocPatchMemory(memory, 1024*64);
     
     // fix bug
@@ -34,3 +84,5 @@ int main() {
     feature.FeatureMarigoldProduceCard();
     feature.FeatureBlover();
 }
+
+#endif
